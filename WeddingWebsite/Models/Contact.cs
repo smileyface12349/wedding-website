aa4e46146279461f7c654e6ee7b5b693cdf3deb1
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-08-20 23:05:32 +0100

    Add contact object for contacts on website

---
+++ WeddingWebsite/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace WeddingWebsite.Models;
+
+/// <summary>
+/// Someone to display in a list of contacts
+/// </summary>
+/// <param name="Role">E.g. "Best Man" or "Father of the Bride"</param>
+/// <param name="ReasonForContacting">E.g. "if you're no longer able to come" or "for enquiries about logistics" </param>
+public record Contact(
+    string FirstName,
+    string LastName,
+    string Role,
+    ContactDetails ContactDetails,
+    string? ReasonForContacting
+) : IContactable
+{
+    public Contact(IContactable contactable, string role, string? reason) : this(contactable.FirstName, contactable.LastName, role, contactable.ContactDetails, reason) {}
+}
