711ea723c3d21ad924d6601b98db9f378daa650f
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-08-20 21:07:52 +0100

    Remove date information from events

--- WeddingWebsite/Models/Event.cs
+++ WeddingWebsite/Models/Event.cs
@@ -1,9 +1,13 @@
 ﻿namespace WeddingWebsite.Models;
 
+/// <summary>
+/// Something to display in the "order of the day".
+/// Note that we are assuming all events take place on the day of the wedding.
+/// </summary>
 public record Event(
     string Name,
-    DateTime Start,
-    DateTime? End,
+    TimeOnly Start,
+    TimeOnly? End,
     string Description,
     IVenue Venue
 );
