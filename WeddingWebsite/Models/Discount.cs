c7d3f09e3be73fcba67e9832afc68e652b800db6
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-08-20 21:31:17 +0100

    Add no discount record

--- WeddingWebsite/Models/Discount.cs
+++ WeddingWebsite/Models/Discount.cs
@@ -9,4 +9,9 @@ public record Discount(
     {
         return originalPrice * (1 - PercentDiscount / 100f);
     }
+    
+    public record None : Discount
+    {
+        public None() : base(0, string.Empty) { }
+    }
 };
