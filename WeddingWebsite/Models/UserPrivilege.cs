e9c8c934194dcab79094d14f5629d24881fd7274
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-08-21 17:49:43 +0100

    Add UserPrivilege enum

---
+++ WeddingWebsite/Models/UserPrivilege.cs
@@ -0,0 +1,25 @@
+﻿namespace WeddingWebsite.Models;
+
+public enum UserPrivilege
+{
+    /// <summary>
+    /// Full access to viewing and updating all users.
+    /// </summary>
+    Admin,
+    
+    /// <summary>
+    /// Guest of reception and ceremony. Can view all information and RSPV.
+    /// </summary>
+    ReceptionGuest,
+    
+    /// <summary>
+    /// Guest of ceremony only. Can view a limited amount of information, and not RSPV.
+    /// You may want to have just one account for ceremony guests.
+    /// </summary>
+    CeremonyGuest,
+    
+    /// <summary>
+    /// A logged-out user.
+    /// </summary>
+    None
+}
