4c4eeb2ed2e602aaa35dffb609e8730f242f9408
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-08-27 21:40:39 +0100

    Add navbar background when scrolling

---
+++ WeddingWebsite/wwwroot/lib/scroll.js
@@ -0,0 +1,7 @@
+﻿window.addEventListener('scroll',function(e) {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.querySelector('.top-row').classList.add('scrolled')
+    } else {
+        document.querySelector('.top-row').classList.remove('scrolled')
+    }
+});
