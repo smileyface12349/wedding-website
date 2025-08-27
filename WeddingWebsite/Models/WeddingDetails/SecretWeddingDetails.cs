89d6ac196bdaaa8ae9a445c00652a545e7617334
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-08-27 20:27:36 +0100

    Add test page for auth

---
+++ WeddingWebsite/Models/WeddingDetails/SecretWeddingDetails.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using WeddingWebsite.Models.People;
+
+namespace WeddingWebsite.Models.WeddingDetails;
+
+/// <summary>
+/// Used to test authorization - should not be accessible to unauthenticated users
+/// </summary>
+
+[Authorize]
+public class SecretWeddingDetails : SampleWeddingDetails
+{
+    public new Fiance Groom { get; } = new Fiance("Spongebob", "Squarepants", new ContactDetails(null, null));
+}
