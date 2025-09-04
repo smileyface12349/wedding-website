9a5915dbcd16e859c0ed9675a8a735b38fcd44a1
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-04 22:41:22 +0100

    Expose url and alt text from website image to allow generating html manually

--- WeddingWebsite/Models/WebsiteElement/WebsiteImage.cs
+++ WeddingWebsite/Models/WebsiteElement/WebsiteImage.cs
@@ -2,8 +2,8 @@
 
 public class WebsiteImage(string url, string? altText) : IWebsiteElement
 {
-    private string Url { get; } = url;
-    private string? AltText { get; } = altText;
+    public string Url { get; } = url;
+    public string? AltText { get; } = altText;
 
     public string GetHtml(string classList = "") {
         return $"<img class=\"{classList}\" src=\"{Url}\" alt=\"{AltText ?? "Image"}\" />";
