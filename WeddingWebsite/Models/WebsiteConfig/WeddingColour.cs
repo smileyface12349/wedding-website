b94152412f63e31741271098946322a277969166
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-06 15:09:38 +0100

    Add classes to store colours

---
+++ WeddingWebsite/Models/WebsiteConfig/WeddingColour.cs
@@ -0,0 +1,21 @@
+﻿using MudBlazor.Utilities;
+
+namespace WeddingWebsite.Models.WebsiteConfig;
+
+public class WeddingColour
+{
+    public MudColor MudColor { get; }
+    
+    public WeddingColour(byte red, byte green, byte blue, byte alpha = 255) {
+        MudColor = new (red, green, blue, alpha);
+    }
+    
+    public WeddingColour(string hex)
+    {
+        MudColor = (MudColor) hex;
+    }
+    
+    public WeddingColour(MudColor mudColor) {
+        MudColor = mudColor;
+    }
+}
---
+++ WeddingWebsite/Models/WebsiteConfig/WeddingColours.cs
@@ -0,0 +1,22 @@
+﻿using MudBlazor.Utilities;
+
+namespace WeddingWebsite.Models.WebsiteConfig;
+
+public record WeddingColours(WeddingColour Primary, WeddingColour PrimaryLight, WeddingColour SurfaceVariant)
+{
+    /// <summary>
+    /// This is the main theme colour of the wedding, the first point of call when any colour is needed.
+    /// This should be dark enough to stand out against a white background.
+    /// </summary>
+    public WeddingColour Primary { get; } = Primary;
+
+    /// <summary>
+    /// A lighter version of the primary colour. This should look good against dark backgrounds.
+    /// </summary>
+    public WeddingColour PrimaryLight { get; } = PrimaryLight;
+
+    /// <summary>
+    /// The default background is white. This colour is used for some colourful backgrounds.
+    /// </summary>
+    public WeddingColour SurfaceVariant { get; } = SurfaceVariant;
+}
