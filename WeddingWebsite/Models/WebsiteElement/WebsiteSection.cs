492d939dfe7afebb37df5dda89a0ce170031fb91
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-07 14:57:51 +0300

    Change modals to use custom WebsiteSection objects instead of html

---
+++ WeddingWebsite/Models/WebsiteElement/WebsiteSection.cs
@@ -0,0 +1,18 @@
+﻿namespace WeddingWebsite.Models.WebsiteElement;
+
+/// <summary>
+/// A section with a heading and content. This deliberately abstracts from how it is rendered.
+/// </summary>
+/// <param name="Heading">Text of high emphasis that appears before the content</param>
+/// <param name="Content">Paragraphs of content appearing after the heading</param>
+/// <param name="Media">Media which may appear anywhere from before the heading to after the content</param>
+public record WebsiteSection (string? Heading, IEnumerable<string> Content, IWebsiteElement? Media = null)
+{
+    /// <summary>
+    /// If you really need an image to be in a certain order, add the image in its own section.
+    /// </summary>
+    public WebsiteSection(IWebsiteElement media) : this(null, [], media) {}
+    
+    public WebsiteSection(string? heading, string? content, IWebsiteElement? media = null) 
+        : this (heading, content == null ? [] : [content], media) {}
+}
--- WeddingWebsite/Models/WebsiteElement/WeddingModal.cs
+++ WeddingWebsite/Models/WebsiteElement/WeddingModal.cs
@@ -1,6 +1,8 @@
 ﻿namespace WeddingWebsite.Models.WebsiteElement;
 
-public record WeddingModal(string Label, string HtmlContent, string Title)
+public record WeddingModal(string Label, IEnumerable<WebsiteSection> Content, string Title)
 {
-    public WeddingModal(string label, string htmlContent) :  this(label, htmlContent, label) {}
+    public WeddingModal(string label, IEnumerable<WebsiteSection> content) :  this(label, content, label) {}
+    
+    public WeddingModal(string label, string content) : this (label, [new WebsiteSection(null, content)], label) {}
 }
