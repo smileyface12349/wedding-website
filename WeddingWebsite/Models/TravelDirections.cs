e8c8295b841dc17d8d2727c89c06619cde43f4ca
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-08 15:21:15 +0300

    Use website sections for travel directions

--- WeddingWebsite/Models/TravelDirections.cs
+++ WeddingWebsite/Models/TravelDirections.cs
@@ -3,7 +3,11 @@
 namespace WeddingWebsite.Models;
 
 public record TravelDirections(
-    string HtmlContent,
+    IEnumerable<WebsiteSection> Content,
     int? DrivingTimeInMinutes = null,
     WebsiteImage? CoverImage = null
-);
+)
+{
+    public TravelDirections(string content, int? drivingTimeInMinutes = null, WebsiteImage? coverImage = null) 
+        : this([new WebsiteSection(null, content)], drivingTimeInMinutes, coverImage) {}
+}
