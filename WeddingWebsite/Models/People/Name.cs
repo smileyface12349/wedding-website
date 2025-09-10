7ef647cf6db7d8f24d25cfcd6ed2957f32eb4a6b
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-10 15:11:21 +0300

    Add new class for first name and last name (instead of an interface)

---
+++ WeddingWebsite/Models/People/Name.cs
@@ -0,0 +1,10 @@
+﻿namespace WeddingWebsite.Models.People;
+
+public record Name(string First, string Last)
+{
+    public string Full => $"{First} {Last}";
+
+    public override string ToString() {
+        return Full;
+    }
+}
