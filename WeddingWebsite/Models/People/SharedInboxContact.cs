1ee9b9379be48bcc000e7d03ce44de425ad9d0bc
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-10 15:12:23 +0300

    Add new type of contact for a shared inbox

---
+++ WeddingWebsite/Models/People/SharedInboxContact.cs
@@ -0,0 +1,14 @@
+﻿using WeddingWebsite.Core;
+
+namespace WeddingWebsite.Models.People;
+
+public record SharedInboxContact(
+    string Title,
+    IEnumerable<Role> Recipients,
+    ContactDetails ContactDetails,
+    string ReasonForContacting = "",
+    bool Emphasise = false
+) : IContact
+{
+    public string NameAndRole => $"{Title} ({string.Join(", ", Recipients.Select(r => r.GetEnumDescription()))})";
+}
