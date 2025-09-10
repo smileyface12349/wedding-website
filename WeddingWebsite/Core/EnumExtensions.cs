ce8cf32c438cf5d09be8dbdbbf81df622eb2a257
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-10 15:12:05 +0300

    Add extension method to get description of enum

---
+++ WeddingWebsite/Core/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace WeddingWebsite.Core;
+
+public static class EnumExtensions
+{
+    public static string GetEnumDescription(this Enum enumValue)
+    {
+        var field = enumValue.GetType().GetField(enumValue.ToString());
+        
+        if (field == null) 
+        {
+            throw new ArgumentException("Item not found.", nameof(enumValue));
+        }
+        
+        if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+        {
+            return attribute.Description;
+        }
+        
+        throw new ArgumentException("Item not found.", nameof(enumValue));
+    }
+}
