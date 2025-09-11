809cd400feada1b96dbf4fcef09156f37cac4fed
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-11 18:35:28 +0300

    Add special catch-all reason for contacting

--- WeddingWebsite/Models/People/ContactOptions.cs
+++ WeddingWebsite/Models/People/ContactOptions.cs
@@ -1,6 +1,19 @@
 ﻿namespace WeddingWebsite.Models.People;
 
-public record ContactOptions(
-    IEnumerable<ContactReason> Reasons,
-    IEnumerable<IContactOption> Methods
-);
+public record ContactOptions
+{
+    /// <summary>
+    /// Null is a special state that matches any reason.
+    /// </summary>
+    private IEnumerable<ContactReason>? Reasons;
+    public IEnumerable<IContactOption> Methods { get; }
+    
+    public ContactOptions(IEnumerable<ContactReason>? reasons, IEnumerable<IContactOption> methods) {
+        Reasons = reasons;
+        Methods = methods;
+    }
+    
+    public bool MatchesReason(ContactReason reason) {
+        return Reasons == null || Reasons.Contains(reason);
+    }
+}
