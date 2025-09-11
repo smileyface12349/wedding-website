6564c365576557c9f2f34d18ed0cb6df2dd4d233
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-11 18:02:46 +0300

    Add enum for possible reasons for contacting

---
+++ WeddingWebsite/Models/People/ContactReason.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace WeddingWebsite.Models.People;
+
+public enum ContactReason
+{
+    [Description("Attendance & RSPVs")]
+    Attendance,
+    
+    [Description("Dietary Requirements & Special Arrangements")]
+    DietaryRequirements,
+    
+    [Description("Accommodation")]
+    Accommodation,
+    
+    [Description("Registry & Gifts")]
+    Registry,
+    
+    [Description("Travel & Logistics")]
+    Logistics,
+    
+    [Description("Website")]
+    Website,
+    
+    [Description("Other Enquiries")]
+    Other,
+    
+    [Description("I want to contact a specific person")]
+    SpecificPerson
+}
