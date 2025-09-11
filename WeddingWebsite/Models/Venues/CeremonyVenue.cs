8eecbcaf4f24cf4b56b9bfc32d5e44016dea8590
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-11 22:02:06 +0300

    Remove unnecessary classes for particular kinds of venues

--- WeddingWebsite/Models/Venues/CeremonyVenue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeddingWebsite.Models.Venues;
-
-public record CeremonyVenue(
-    string Name,
-    Location Location,
-    string Address,
-    TravelDirections Directions
-) : IVenue;
--- WeddingWebsite/Models/Venues/ReceptionVenue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeddingWebsite.Models.Venues;
-
-public record ReceptionVenue(
-    string Name,
-    Location Location,
-    string Address,
-    TravelDirections? Directions
-) : IVenue;
