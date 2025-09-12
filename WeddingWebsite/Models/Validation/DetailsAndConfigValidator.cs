f9b187edf11ab470d1b5138281f1a3032cf4630f
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-12 16:26:26 +0300

    Add skeleton validator

---
+++ WeddingWebsite/Models/Validation/DetailsAndConfigValidator.cs
@@ -0,0 +1,16 @@
+﻿using WeddingWebsite.Models.WebsiteConfig;
+using WeddingWebsite.Models.WeddingDetails;
+
+namespace WeddingWebsite.Models.Validation;
+
+public class DetailsAndConfigValidator: IDetailsAndConfigValidator
+{
+    private IList<ValidationIssue> validationIssues = [];
+    
+    public IEnumerable<ValidationIssue> Validate(IWeddingDetails details, IWebsiteConfig config) {
+        validationIssues = [];
+        
+
+        return validationIssues;
+    }
+}
