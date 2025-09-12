61a297ab3647d1fec9e3d716ee9b21ce2703bc14
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-12 16:30:03 +0300

    Add helper methods to record validation issues

--- WeddingWebsite/Models/Validation/DetailsAndConfigValidator.cs
+++ WeddingWebsite/Models/Validation/DetailsAndConfigValidator.cs
@@ -13,4 +13,26 @@ public class DetailsAndConfigValidator: IDetailsAndConfigValidator
 
         return validationIssues;
     }
+    
+    /// <summary>
+    /// An error that leads to incorrect / misleading information, or severe and definitely undesired behaviour.
+    /// These errors must still be recoverable i.e. the site should render without throwing.
+    /// </summary>
+    private void Error(string message) {
+        validationIssues.Add(new ValidationIssue(ValidationIssueSeverity.Error, message));
+    }
+    
+    /// <summary>
+    /// An issue that seems weird, but may be valid. E.g. specifying data that's overridden by another setting.
+    /// </summary>
+    private void Warning(string message) {
+        validationIssues.Add(new ValidationIssue(ValidationIssueSeverity.Warning, message));
+    }
+    
+    /// <summary>
+    /// Something worth flagging, but very unlikely to cause a problem.
+    /// </summary>
+    private void Info(string message) {
+        validationIssues.Add(new ValidationIssue(ValidationIssueSeverity.Info, message));
+    }
 }
