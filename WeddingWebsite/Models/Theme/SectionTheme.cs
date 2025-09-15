49bcdb446226da20daf918eae8d12d3626cf7bd5
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-15 15:43:31 +0100

    Add class for section theme

---
+++ WeddingWebsite/Models/Theme/SectionTheme.cs
@@ -0,0 +1,9 @@
+﻿using WeddingWebsite.Models.WebsiteConfig;
+
+namespace WeddingWebsite.Models.Theme;
+
+public sealed record SectionTheme(
+    Colour Background,
+    Colour Primary,
+    BoxStyle BoxStyle
+);
