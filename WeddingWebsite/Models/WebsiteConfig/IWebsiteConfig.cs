3894f731d7fe6fd795596688597fc8e8dcb628a9
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-16 00:01:25 +0100

    Update comment for website config

--- WeddingWebsite/Models/WebsiteConfig/IWebsiteConfig.cs
+++ WeddingWebsite/Models/WebsiteConfig/IWebsiteConfig.cs
@@ -3,8 +3,10 @@
 namespace WeddingWebsite.Models.WebsiteConfig;
 
 /// <summary>
-/// Configuration options. None of this information should be sensitive, and it should be stuff that's personal
-/// preference rather than details about a particular wedding.
+/// Configuration options. None of this information should be sensitive, and it should be stuff that relates to
+/// configuring the website rather than anything related to a particular wedding.
+///
+/// Configuration options that relate to one section only can be found within that particular section's constructor.
 /// </summary>
 
 public interface IWebsiteConfig
