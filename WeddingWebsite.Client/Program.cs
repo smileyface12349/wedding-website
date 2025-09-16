86cfdf8ffce8ca9421a257d8b4fde0c6f20039cb
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-16 20:22:12 +0100

    Move client project into non-nested directory

--- WeddingWebsite.Client/Program.cs
+++ WeddingWebsite.Client/Program.cs
@@ -2,15 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WeddingWebsite.Client.Config;
 
-namespace Client;
+namespace WeddingWebsite.Client;
 
 public class Program
 {
     public static async Task Main(string[] args) {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<HeadOutlet>("head::after");
-
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        
         builder.Services.AddScoped<IWebsiteConfig, WebsiteConfig>();
 
         await builder.Build().RunAsync();
