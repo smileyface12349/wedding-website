bb2d6b38786d005c60fe71822fc3a229e8f185de
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-18 23:28:31 +0100

    Apply correct navbar scrolled status when page is loaded after already scrolled

--- WeddingWebsite/wwwroot/lib/scroll.js
+++ WeddingWebsite/wwwroot/lib/scroll.js
@@ -1,7 +1,15 @@
-﻿window.addEventListener('scroll',function(e) {
+﻿function onScroll(e) {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.querySelector('.top-row').classList.add('scrolled')
     } else {
         document.querySelector('.top-row').classList.remove('scrolled')
     }
-});
+}
+
+function delayScroll(e) {
+    // TODO: This is to combat blazor refreshing the component server-side, sending an incorrect version back. Need to work through a better solution.
+    setTimeout(onScroll, 250);
+}
+
+window.addEventListener('scroll', onScroll);
+window.addEventListener('load', delayScroll);
