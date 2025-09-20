6ad272e121027a20fda2235b66820ed6aa9be44f
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2025-09-20 21:23:47 +0100

    Implement parallax option for background images

--- WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
+++ WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
@@ -1,11 +1,21 @@
 ﻿namespace WeddingWebsite.Models.WebsiteConfig;
 
-public record BackgroundImage(string Url, bool IsDark, string Width = "100%", Colour? OverlayColour = null) : IBackground
+public record BackgroundImage(string Url, bool IsDark, string Width = "100%", Colour? OverlayColour = null, float ParallaxPercent = 0) : IBackground
 {
     public Colour GetTextColour() => IsDark ? Colour.White : Colour.Black;
     public string GetBackgroundCss() {
-        var overlay = OverlayColour != null ? $"box-shadow: inset 0 0 0 2000px {OverlayColour};" : "";
-        return $"background-image: url('{Url}'); background-size: {Width} auto;{overlay}";
+        var css = $"background-image: url('{Url}'); background-size: {Width} auto;";
+        if (OverlayColour != null) {
+            css += $"box-shadow: inset 0 0 0 2000px {OverlayColour};";
+        }
+        if (ParallaxPercent != 0) {
+            if (ParallaxPercent >= 0.99) {
+                css += "background-attachment: fixed;";
+            } else {
+                throw new NotImplementedException();
+            }
+        }
+        return css;
     } 
     
 }
