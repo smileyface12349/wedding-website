e37c9f5df7a5e688f30d1032c52fd4bcee1c8032
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-21 22:50:42 +0100

    Add property IsFractionalParallax to BackgroundImage

--- WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
+++ WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
@@ -2,7 +2,8 @@
 
 public record BackgroundImage(string Url, bool IsDark, string Width = "100%", Colour? OverlayColour = null, double Parallax = 0, bool ExtraContrast = false) : IBackground
 {
-    public bool HasPureCssImplementation => Parallax < 0.001 || Parallax > 0.999;
+    public bool IsFractionalParallax => Parallax is > 0.001 or < 0.999;
+    public bool HasPureCssImplementation => !IsFractionalParallax;
     public Colour GetTextColour() => IsDark ? Colour.White : Colour.Black;
     public string GetBackgroundCss() {
         var css = $"background-image: url('{Url}'); background-size: {Width} auto;";
