b7e911f6e37336dc63e8aa05f1120e9652c3fef3
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-21 23:08:23 +0100

    Add fractional parallax background

---
+++ WeddingWebsite/Components/Sections/Backgrounds/FractionalParallaxBackground.razor.js
@@ -0,0 +1,22 @@
+﻿
+
+function onScroll(e) {
+    console.log("Scrolled, new offset: " + getOffset());
+    document.querySelector('#background').style="background-position: top " + getOffset() + "px left 0px";
+}
+
+function getOffset() {
+    let percent = parseFloat(document.getElementById("background-scroll-amount").innerHTML);
+    let scrollDistance = document.documentElement.scrollTop;
+    return scrollDistance * percent;
+}
+
+export function onLoad() {
+    onScroll();
+}
+
+export function onUpdate() {
+    onScroll();
+}
+
+window.addEventListener('scroll', onScroll);
