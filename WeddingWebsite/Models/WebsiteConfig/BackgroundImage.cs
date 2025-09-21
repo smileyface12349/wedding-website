ace3be27ecca0ead3b70925e22261dfee99a889e
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-21 22:48:05 +0100

    Update BackgroundImage to support fractional parallax without using a separate class

--- WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
+++ WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
@@ -1,15 +1,15 @@
 ﻿namespace WeddingWebsite.Models.WebsiteConfig;
 
-public record BackgroundImage(string Url, bool IsDark, string Width = "100%", Colour? OverlayColour = null, bool Parallax = false, bool ExtraContrast = false) : IBackground
+public record BackgroundImage(string Url, bool IsDark, string Width = "100%", Colour? OverlayColour = null, double Parallax = 0, bool ExtraContrast = false) : IBackground
 {
-    public bool HasPureCssImplementation => true;
+    public bool HasPureCssImplementation => Parallax < 0.001 || Parallax > 0.999;
     public Colour GetTextColour() => IsDark ? Colour.White : Colour.Black;
     public string GetBackgroundCss() {
         var css = $"background-image: url('{Url}'); background-size: {Width} auto;";
         if (OverlayColour != null) {
             css += $"box-shadow: inset 0 0 0 2000px {OverlayColour};";
         }
-        if (Parallax) {
+        if (Parallax > 0.999) {
             css += "background-attachment: fixed;";
         } 
         return css;
