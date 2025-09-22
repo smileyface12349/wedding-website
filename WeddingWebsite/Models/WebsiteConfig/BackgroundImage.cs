d4b0237177c6da62c2963d7a55c9e1c5d96284dc
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-22 10:33:41 +0100

    Handle values outside of [0, 1] as fractional parallax

--- WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
+++ WeddingWebsite/Models/WebsiteConfig/BackgroundImage.cs
@@ -2,7 +2,7 @@
 
 public record BackgroundImage(string Url, bool IsDark, string Width = "100%", Colour? OverlayColour = null, double Parallax = 0, bool ExtraContrast = false) : IBackground
 {
-    public bool IsFractionalParallax => Parallax is > 0.001 or < 0.999;
+    public bool IsFractionalParallax => Parallax != 0 && Math.Abs(Parallax - 1) > 0.0001;
     public bool HasPureCssImplementation => !IsFractionalParallax;
     public Colour GetTextColour() => IsDark ? Colour.White : Colour.Black;
     public string GetBackgroundCss() {
@@ -10,7 +10,7 @@ public record BackgroundImage(string Url, bool IsDark, string Width = "100%", Co
         if (OverlayColour != null) {
             css += $"box-shadow: inset 0 0 0 2000px {OverlayColour};";
         }
-        if (Parallax > 0.999) {
+        if (Math.Abs(Parallax - 1) <= 0.0001) {
             css += "background-attachment: fixed;";
         } 
         return css;
