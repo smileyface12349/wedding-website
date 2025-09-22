a100efa9a8d779ed21fd1b275acf1262cd56c9e9
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-22 11:33:16 +0100

    Add optional image to venue

--- WeddingWebsite/Models/Venues/Venue.cs
+++ WeddingWebsite/Models/Venues/Venue.cs
@@ -1,6 +1,8 @@
-﻿namespace WeddingWebsite.Models.Venues;
+﻿using WeddingWebsite.Models.WebsiteElement;
+
+namespace WeddingWebsite.Models.Venues;
 
 /// <summary>
 /// A venue that stuff happens in.
 /// </summary>
-public record Venue (string Name, Location Location, string Address, TravelDirections? Directions = null);
+public record Venue (string Name, Location Location, string Address, TravelDirections? Directions = null, IWebsiteElement? Media = null);
