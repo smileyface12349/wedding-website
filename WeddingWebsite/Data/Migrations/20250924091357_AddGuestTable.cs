b776b5f822f49dcd82b54aa083cda51808667105
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-24 10:58:37 +0100

    Add database migration

---
+++ WeddingWebsite/Data/Migrations/20250924091357_AddGuestTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using WeddingWebsite.Data.Enums;
+using WeddingWebsite.Models;
+
+#nullable disable
+
+namespace WeddingWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGuestTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    GuestId = table.Column<string>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    RsvpStatus = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => new { x.GuestId });
+                    table.ForeignKey(
+                        name: "FK_Guests_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Guests");
+        }
+    }
+}
