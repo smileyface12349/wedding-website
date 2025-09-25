4f1a5efefdd340885b4758f905c48f2765a372d4
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-25 11:07:53 +0100

    Add new model for account with guests

--- WeddingWebsite/Data/Account.cs
+++ WeddingWebsite/Data/Account.cs
@@ -3,4 +3,8 @@ using WeddingWebsite.Models;
 
 namespace WeddingWebsite.Data;
 
+/// <summary>
+/// The bare minimum necessary information for the authentication system. Guests are not fetched as standard, and are
+/// instead in <see cref="Models.AccountWithGuests"/>
+/// </summary>
 public class Account : IdentityUser;
