b79b73e303b6fabeeb4bd5946275391d1f0cb2eb
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-25 15:47:09 +0100

    Refactor to move GuestResponse to controller and use GuestWithId instead

---
+++ WeddingWebsite/Data/Models/GuestWithId.cs
@@ -0,0 +1,6 @@
+﻿using WeddingWebsite.Models;
+using WeddingWebsite.Models.People;
+
+namespace WeddingWebsite.Data.Models;
+
+public record GuestWithId(string Id, Name Name, RsvpStatus RsvpStatus) : Guest(Name, RsvpStatus);
