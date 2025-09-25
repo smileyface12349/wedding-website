24fbe2dde0f7e9726bf0b4cd18cd23c0aad2fa65
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-25 11:09:02 +0100

    Add service method to get all accounts

--- WeddingWebsite/Services/AdminService.cs
+++ WeddingWebsite/Services/AdminService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using WeddingWebsite.Data.Models;
 using WeddingWebsite.Data.Stores;
 
 namespace WeddingWebsite.Services;
@@ -9,5 +10,10 @@ public class AdminService(IStore store) : IAdminService
     public void AddGuestToAccount(string userId, string firstName, string lastName)
     {
         store.AddGuestToAccount(userId, firstName, lastName);
+    }
+
+    public IEnumerable<AccountWithGuests> GetAllAccounts()
+    {
+        return store.GetAllAccounts();
     }
 }
--- WeddingWebsite/Services/IAdminService.cs
+++ WeddingWebsite/Services/IAdminService.cs
@@ -1,6 +1,9 @@
-﻿namespace WeddingWebsite.Services;
+﻿using WeddingWebsite.Data.Models;
+
+namespace WeddingWebsite.Services;
 
 public interface IAdminService
 {
     void AddGuestToAccount(string userId, string firstName, string lastName);
+    IEnumerable<AccountWithGuests> GetAllAccounts();
 }
