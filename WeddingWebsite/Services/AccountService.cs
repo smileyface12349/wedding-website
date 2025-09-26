c9914bcf888617485a0bd6aa6fe349fc82db6681
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-26 14:24:33 +0100

    Add service method to add account logs

--- WeddingWebsite/Services/AccountService.cs
+++ WeddingWebsite/Services/AccountService.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using WeddingWebsite.Data.Models;
 using WeddingWebsite.Data.Stores;
+using WeddingWebsite.Models.Accounts;
 
 namespace WeddingWebsite.Services;
 
@@ -10,8 +11,29 @@ public class AccountService(IStore store) : IAccountService
 {
     public IEnumerable<GuestWithId> GetOwnGuests(ClaimsPrincipal user)
     {
+        return store.GetGuestsForUser(GetUserId(user));
+    }
+    
+    public void Log(ClaimsPrincipal user, AccountLogType logType, string description, string? affectedUserId = null)
+    {
+        var affectedUser = affectedUserId ?? GetUserId(user);
+        store.AddAccountLog(affectedUser, GetUserId(user), logType, description);
+    }
+
+    private string? GetUserIdOrNull(ClaimsPrincipal user)
+    {
         var claim = user.FindFirst(ClaimTypes.NameIdentifier);
-        var userId = claim.Value;
-        return store.GetGuestsForUser(userId);
+        return claim?.Value;
+    }
+    
+    private string GetUserId(ClaimsPrincipal user)
+    {
+        var userId = GetUserIdOrNull(user);
+        if (userId == null)
+        {
+            throw new InvalidOperationException("User ID claim is missing.");
+        }
+
+        return userId;
     }
 }
--- WeddingWebsite/Services/IAccountService.cs
+++ WeddingWebsite/Services/IAccountService.cs
@@ -1,9 +1,19 @@
 ﻿using System.Security.Claims;
 using WeddingWebsite.Data.Models;
+using WeddingWebsite.Models.Accounts;
 
 namespace WeddingWebsite.Services;
 
 public interface IAccountService
 {
     public IEnumerable<GuestWithId> GetOwnGuests(ClaimsPrincipal user);
+
+    /// <summary>
+    /// Log a new event relating to a user account.
+    /// </summary>
+    /// <param name="user">The currently logged in user.</param>
+    /// <param name="logType">What type of event happened.</param>
+    /// <param name="description">A short description of what happened with any extra info that may be useful.</param>
+    /// <param name="affectedUserId">(Optional) Which user is affected by this action. Defaults to the logged in user.</param>
+    public void Log(ClaimsPrincipal user, AccountLogType logType, string description, string? affectedUserId = null);
 }
