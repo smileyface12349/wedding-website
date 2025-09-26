35bb549b9f865b34de25666da999305b4d6752a4
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-26 14:14:41 +0100

    Add account log type enum with enum converter

---
+++ WeddingWebsite/Data/Enums/AccountLogTypeEnumConverter.cs
@@ -0,0 +1,31 @@
+﻿using WeddingWebsite.Models.Accounts;
+
+namespace WeddingWebsite.Data.Enums;
+
+public static class AccountLogTypeEnumConverter
+{
+    public static int ToDatabaseInteger(this AccountLogType accountLogType)
+    {
+        return accountLogType switch
+        {
+            AccountLogType.LogIn => 0,
+            AccountLogType.ChangePassword => 1,
+            _ => throw new ArgumentOutOfRangeException(nameof(accountLogType), accountLogType, null)
+        };
+    }
+    
+    public static int AccountLogTypeToDatabaseInteger(AccountLogType accountLogType)
+    {
+        return accountLogType.ToDatabaseInteger();
+    }
+    
+    public static AccountLogType DatabaseIntegerToAccountLogType(int accountLogType)
+    {
+        return accountLogType switch
+        {
+            0 => AccountLogType.LogIn,
+            1 => AccountLogType.ChangePassword,
+            _ => throw new ArgumentOutOfRangeException(nameof(accountLogType), accountLogType, null)
+        };
+    }
+}
---
+++ WeddingWebsite/Models/Accounts/AccountLogType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace WeddingWebsite.Models.Accounts;
+
+public enum AccountLogType
+{
+    [Description("Log in")]
+    LogIn,
+    
+    [Description("Change password")]
+    ChangePassword,
+}
