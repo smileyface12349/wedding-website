077058d1e9d271d0a8818a4e366e12c1539b541a
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-09-28 21:25:36 +0100

    Add record for account logs

---
+++ WeddingWebsite/Models/Accounts/AccountLog.cs
@@ -0,0 +1,11 @@
+﻿using WeddingWebsite.Data;
+
+namespace WeddingWebsite.Models.Accounts;
+
+public record AccountLog(
+    DateTime Date,
+    Account AffectedUser,
+    Account Actor,
+    AccountLogType LogType,
+    string Description
+);
