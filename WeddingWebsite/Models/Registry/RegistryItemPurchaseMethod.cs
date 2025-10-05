c99b17e008cd4c0b56d431c9067eba64d437f841
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-10-05 11:43:32 +0100

    Replace double with decimal in purchase method

--- WeddingWebsite/Models/Registry/RegistryItemPurchaseMethod.cs
+++ WeddingWebsite/Models/Registry/RegistryItemPurchaseMethod.cs
@@ -3,10 +3,10 @@
 public record RegistryItemPurchaseMethod(
     string Id,
     string Name,
-    double Cost,
+    decimal Cost,
     bool AllowBringOnDay = true,
     bool AllowDeliverToUs = true,
     string? Url = null,
     string? Instructions = null,
-    double DeliveryCost = 0
+    decimal DeliveryCost = 0
 );
