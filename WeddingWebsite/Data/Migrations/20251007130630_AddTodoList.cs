048e46be858386ef966a6a2b2dd50881f40c6145
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-10-07 14:12:24 +0100

    Add new tables

---
+++ WeddingWebsite/Data/Migrations/20251007130630_AddTodoList.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTodoList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<string>(type: "TEXT", nullable: true),
+                    GroupId = table.Column<string>(type: "TEXT", nullable: true),
+                    Text = table.Column<string>(type: "TEXT", nullable: true),
+                    WaitingUntil = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CompletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                    table.ForeignKey("FK_TodoItems_AspNetUsers_OwnerId", x => x.OwnerId, "AspNetUsers", "Id");
+                    table.ForeignKey("FK_TodoItems_TodoGroups_GroupId", x => x.GroupId, "TodoGroups", "Id");
+                });
+            
+            migrationBuilder.CreateTable(
+                name: "TodoGroups",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoGroups", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("TodoItems");
+            migrationBuilder.DropTable("TodoGroups");
+        }
+    }
+}
