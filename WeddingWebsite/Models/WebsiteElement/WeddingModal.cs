2bd6eafee8649395ff2571bb36b4fe8b4f24b8ee
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-10-12 11:27:17 +0100

    Add optional actions to wedding modal

--- WeddingWebsite/Models/WebsiteElement/WeddingModal.cs
+++ WeddingWebsite/Models/WebsiteElement/WeddingModal.cs
@@ -1,10 +1,10 @@
 ﻿namespace WeddingWebsite.Models.WebsiteElement;
 
-public record WeddingModal(string Label, IEnumerable<WebsiteSection> Content, string Title)
+public record WeddingModal(string Label, IEnumerable<WebsiteSection> Content, string Title, IEnumerable<LinkButton> Actions)
 {
-    public WeddingModal(string label, IEnumerable<WebsiteSection> content) :  this(label, content, label) {}
+    public WeddingModal(string label, IEnumerable<WebsiteSection> content) :  this(label, content, label, []) {}
     
-    public WeddingModal(string label, string content) : this (label, [new WebsiteSection(null, content)], label) {}
+    public WeddingModal(string label, string content) : this (label, [new WebsiteSection(null, content)], label, []) {}
     
-    public WeddingModal(string label, string content, string title) : this (label, [new WebsiteSection(null, content)], title) {}
+    public WeddingModal(string label, string content, string title) : this (label, [new WebsiteSection(null, content)], title, []) {}
 }
