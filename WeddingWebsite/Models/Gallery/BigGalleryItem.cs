991b259eaefdca0b389a311affb31fbc29ad6dbf
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-10-13 15:03:15 +0100

    Add new model for items to show in the section

---
+++ WeddingWebsite/Models/Gallery/BigGalleryItem.cs
@@ -0,0 +1,15 @@
+﻿using WeddingWebsite.Models.WebsiteElement;
+
+namespace WeddingWebsite.Models.Gallery;
+
+/// <summary>
+/// A gallery item for the section on the homepage, which also includes an optional title and description.
+/// </summary>
+/// <param name="Image">The image</param>
+/// <param name="Title">(Optional) Title (you can still specify description without a title)</param>
+/// <param name="Description">(Optional) Description (e.g. author credit)</param>
+public record BigGalleryItem(
+    WebsiteImage Image,
+    string Title = "", 
+    string Description = ""
+);
