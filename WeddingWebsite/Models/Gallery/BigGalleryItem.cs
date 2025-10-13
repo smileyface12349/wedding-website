49bb4ea5a8bb68308bb74ac93ad87ea4753543de
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-10-13 15:46:48 +0100

    Re-order parameters to have credit at the beginning

--- WeddingWebsite/Models/Gallery/BigGalleryItem.cs
+++ WeddingWebsite/Models/Gallery/BigGalleryItem.cs
@@ -6,12 +6,12 @@ namespace WeddingWebsite.Models.Gallery;
 /// A gallery item for the section on the homepage, which also includes an optional title and description.
 /// </summary>
 /// <param name="Image">The image</param>
+/// <param name="Credit">(Optional) E.g. "Credit: John Smith". A bit less invasive than the description.</param>
 /// <param name="Title">(Optional) Title (you can still specify description without a title)</param>
 /// <param name="Description">(Optional) Description</param>
-/// <param name="Credit">(Optional) E.g. "Credit: John Smith". A bit less invasive than the description.</param>
 public record BigGalleryItem(
     WebsiteImage Image,
+    string Credit = "",
     string Title = "", 
-    string Description = "",
-    string Credit = ""
+    string Description = ""
 );
