7098452f8bee913c3c87bb2f5dad268abd9f98a6
smileyface12349 <josh.humphriss.signup@gmail.coom>
2025-11-07 23:22:15 +0000

    Add new price model

---
+++ WeddingWebsite/Models/Accommodation/Price.cs
@@ -0,0 +1,22 @@
+﻿namespace WeddingWebsite.Models.Accommodation;
+
+public record Price(
+    decimal Amount,
+    Discount Discount,
+    string? CustomString = null
+)
+{
+    public Price(decimal amount) : this(amount, Discount.None()) {}
+
+    public override string ToString() {
+        if (CustomString != null) {
+            return CustomString;
+        } else if (Discount.PercentDiscount == 0) {
+            return $"£{Amount}";
+        } else {
+            return $"Full Price: £{Amount}, With Discount: £{Discount.CalculateDiscountedPrice((float) Amount)} ({Discount.ClaimInstructions})";
+        }
+    }
+    
+    public float DiscountedPrice => Discount.CalculateDiscountedPrice((float) Amount);
+}
