1a0f0db0f03baf94fff428f6625c0cf7ddc9039f
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-01-12 23:06:07 +0000

    Update defaults for timeline and dress code sections

--- WeddingWebsite/Models/WebsiteConfig/Section.cs
+++ WeddingWebsite/Models/WebsiteConfig/Section.cs
@@ -25,7 +25,7 @@ public abstract record Section
     /// Unlike most sections, the heading is disabled by default. This is because it looks nice with the timeline
     /// running right to the edge of the section, and the purpose of the section is clear already.
     /// </summary>
-    public sealed record Timeline(SectionTheme? Theme = null, bool ShowHeading = false, bool ShowTravelDirections = true) : Section(Theme);
+    public sealed record Timeline(SectionTheme? Theme = null, bool ShowHeading = true, bool ShowTravelDirections = true) : Section(Theme);
     
     /// <summary>
     /// A very basic version of the timeline that just displays the start time of each event. Does not show travel
@@ -67,7 +67,7 @@ public abstract record Section
     public sealed record DressCode(
         SectionTheme? Theme = null, 
         bool WrapInBox = true,
-        bool ShowContact = true
+        bool ShowContact = false
     ) : Section(Theme);
 
     /// <summary>
