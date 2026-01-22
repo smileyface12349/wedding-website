1b5e01aa7191842e696ba214ba381397f85c6a3b
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-01-22 14:43:49 +0000

    Update NoCredentials to fail gracefully

--- WeddingWebsite/Config/Credentials/NoCredentials.cs
+++ WeddingWebsite/Config/Credentials/NoCredentials.cs
@@ -3,13 +3,11 @@
 namespace WeddingWebsite.Config.Credentials;
 
 /// <summary>
-/// Default credentials file with no credentials. If you try to access something requiring an API key, a
-/// NotImplementedException will be thrown.
-/// However, you do not have to fill in all the credentials if you do not use all the functionality.
-/// This is provided so you can get the code to compile without all the credentials.
+/// Implementation with no credentials, designed to fail gracefully.
+/// If you want to use any restricted functionality, create your own implementation of ICredentials.
 /// </summary>
 
 public class NoCredentials : ICredentials
 {
-    public string GoogleMaps => throw new NotImplementedException();
+    public string GoogleMaps => "";
 }
