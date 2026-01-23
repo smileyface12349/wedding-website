83370e98ca8c317cee9305804a5fedfb846fe9e6
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-01-23 12:34:27 +0000

    Add template file for changing strings

---
+++ WeddingWebsite/Config/Strings/CustomStrings.cs
@@ -0,0 +1,9 @@
+﻿namespace WeddingWebsite.Config.Strings;
+
+public class CustomStrings : FriendlyBritishEnglish
+{
+    // TODO: Change this text
+    public new string RegistryDescription1 => "Please do not feel under any pressure to give a gift - there is no obligation at all to do so! However, if you would like to give a gift, this page contains some suggestions of things we'd like. If you'd prefer to give something else, that's fine too!";
+    public new string RegistryDescription2 => "You can click on an item to view more information about it. Once you've decided to purchase it, please claim it so that we don't get multiple people buying the same thing!";
+    public new string DoNotPurchaseBeforeClaiming => "Please do not make a purchase before claiming the item first - this will reserve it so we don't get two people buying the same thing!";
+}
