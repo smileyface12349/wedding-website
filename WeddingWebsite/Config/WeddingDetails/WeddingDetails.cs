d85e86f6778e942b03f008096f2a7b52258ced0b
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-01-23 12:48:13 +0000

    Cleanup some unused stuff in wedding details

--- WeddingWebsite/Config/WeddingDetails/WeddingDetails.cs
+++ WeddingWebsite/Config/WeddingDetails/WeddingDetails.cs
@@ -9,19 +9,6 @@ using WeddingWebsite.Models.WebsiteElement;
 
 namespace WeddingWebsite.Config.WeddingDetails;
 
-/// <summary>
-/// This file provides a git-tracked up-to-date selection of data suitable for development.
-/// 
-/// This is useful for:
-///  - Trying out the website without needing to enter all your own information first.
-///  - Seeing an example as this can be helpful when constructing your own implementation.
-///  - Sharing screenshots of layout without revealing anything about your wedding.
-///  - A standardized test environment to diagnose problems.
-///  - Updates automatically with new releases so you can test out new releases before filling in details.
-///  - Will facilitate any future unit testing that may be added.
-///
-/// In practice, you won't actually be using this implementation but I'd recommend keeping it around in case you need it.
-/// </summary>
 
 public sealed class WeddingDetails : IWeddingDetails
 {
@@ -113,9 +100,7 @@ public sealed class WeddingDetails : IWeddingDetails
                 new WebsiteSection(null, "Jacob finished studying at Warwick, now serving the church for a year before discovering what the next stage in life is..."),
                 new WebsiteSection("Hobbies", "Music, Stories and Walking."),
                 new WebsiteSection("Fun Fact", "I like cheese.")
-            ],
-            new WebsiteImage("https://upload.wikimedia.org/wikipedia/commons/7/7a/SpongeBob_SquarePants_character.png", null),
-            "124 Conch Street, Bikini Bottom, Pacific Ocean"
+            ]
         ),
         new (
             new Name("Amelia", "Kinghorn"),
@@ -128,102 +113,31 @@ public sealed class WeddingDetails : IWeddingDetails
                 new WebsiteSection(null, "Amelia is Amelia. She loves being herself. She's finished Maths at Warwick and is now working at Dennis Eagle as a Data Analyst mixed with something else."),
                 new WebsiteSection("Hobbies", "Walking, puzzling and napping."),
                 new WebsiteSection("Fun Fact", "IDK yet")
-            ],
-            new WebsiteImage("https://static.wikitide.net/greatcharacterswiki/thumb/5/5c/Original_scooby_doo.png/300px-Original_scooby_doo.png", null),
-            "32 Mystery Lane, Coolsville, USA"
+            ]
         ),
-        //new (
-        //    new Name("John", "Smith"),
-        //    Role.BestMan,
-        //    new ContactDetails(
-        //        new ContactOptions([ContactReason.Attendance, ContactReason.SpecificPerson], [new EmailAddress("john.smith@gmail.com"), new EmailAddress("john.alt@gmail.com")])
-        //    ),
-        //    [
-        //        new WebsiteSection(null, "John is the groom's childhood best friend. They met in primary school and have been inseparable ever since. John is known for his quick wit and sense of humor."),
-        //        new WebsiteSection("Hobbies", "Playing football, video games, and hiking."),
-        //        new WebsiteSection("Fun Fact", "John once won a local stand-up comedy competition.")
-        //    ],
-        //    new WebsiteImage("https://static.vecteezy.com/system/resources/previews/041/642/170/non_2x/ai-generated-portrait-of-handsome-smiling-young-man-with-folded-arms-isolated-free-png.png", null)
-        //),
         new (
             new Name("Alice", "Humphriss"),
             Role.MaidOfHonour,
             new ContactDetails(
                 new ContactOptions([ContactReason.Website, ContactReason.SpecificPerson], [new EmailAddress("IDK")])
-            ),
-            [
-                new WebsiteSection(null, "Sally is the bride's sister and best friend. They share a love for fashion and shopping. Sally is always there to lend a helping hand and offer support."),
-                new WebsiteSection("Hobbies", "Shopping, yoga, and baking."),
-                new WebsiteSection("Fun Fact", "This stuff is being completely AI written!")
-            ],
-            new WebsiteImage("https://png.pngtree.com/png-vector/20240528/ourmid/pngtree-front-view-of-a-smiling-business-woman-png-image_12509704.png", null)
+            )
         ),
         new (
             new Name("Matthew", "Taylor"),
-            Role.Groomsman,
-            new ContactDetails(
-                new ContactOptions([ContactReason.SpecificPerson], [new EmailAddress("mike.davis@gmail.com")])
-            ),
-            [
-                new WebsiteSection(null, "Mike is the groom's cousin and a loyal friend. He has a great sense of adventure and loves trying new things. Mike is always up for a challenge."),
-                new WebsiteSection("Hobbies", "Rock climbing, traveling, and photography."),
-                new WebsiteSection("Fun Fact", "Mike has traveled to over 20 countries.")
-            ],
-            new WebsiteImage("https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-in-shirt-smiles-and-gives-thumbs-up-to-show-approval-png-image_13146336.png", null)
+            Role.Groomsman
         ),
         new (
             new Name("Alicia", "Warner"),
-            Role.Bridesmaid,
-            new ContactDetails(
-                new ContactOptions([ContactReason.SpecificPerson], [new EmailAddress("emily.johnson@gmail.com")])
-            ),
-            [
-                new WebsiteSection(null, "Emily is the bride's childhood friend. They met in primary school and have been inseparable ever since. Emily is known for her kindness and generosity."),
-                new WebsiteSection("Hobbies", "Reading, painting, and gardening."),
-                new WebsiteSection("Fun Fact", "Emily once participated in a flash mob dance performance.")
-            ],
-            new WebsiteImage("https://static.vecteezy.com/system/resources/thumbnails/050/817/792/small_2x/happy-smiling-business-woman-in-suit-with-hand-pointing-at-empty-space-standing-isolate-on-transparent-background-png.png", null)
+            Role.Bridesmaid
         ),
         new (
             new Name("Isabelle", "Kinghorn"),
-            Role.Bridesmaid,
-            new ContactDetails(
-                new ContactOptions([ContactReason.SpecificPerson], [new EmailAddress("jane.butters@gmail.com")])
-            ),
-            [
-                new WebsiteSection(null, "Jane is the bride's college roommate and a close friend. They bonded over their love for music and art. Jane is always up for a good time and loves to make people laugh."),
-                new WebsiteSection("Hobbies", "Playing guitar, attending concerts, and hiking."),
-                new WebsiteSection("Fun Fact", "Jane can play three different musical instruments.")
-            ],
-            new WebsiteImage("https://static.vecteezy.com/system/resources/previews/009/257/276/non_2x/portrait-of-beautiful-young-asian-woman-file-png.png", null)
+            Role.Bridesmaid
         ),
         new (
             new Name("Nathan", "Solomon"),
-            Role.Groomsman,
-            new ContactDetails(
-                new ContactOptions([ContactReason.SpecificPerson], [new EmailAddress("bob.marley@gmail.com")])
-            ),
-            [
-                new WebsiteSection(null, "Bob is the groom's work colleague and a great friend. They met at a company event and hit it off immediately. Bob is known for his positive attitude and infectious laughter."),
-                new WebsiteSection("Hobbies", "Playing basketball, cooking, and fishing."),
-                new WebsiteSection("Fun Fact", "Bob once cooked a meal for a celebrity chef.")
-            ],
-            new WebsiteImage("https://americanmigrainefoundation.org/wp-content/uploads/2022/12/GettyImages-1345864068.png", null)
+            Role.Groomsman
         ),
-        //new (
-        //    new Name("Jim", "Brown"),
-        //    Role.Photographer,
-        //    new ContactDetails(
-        //        new ContactOptions([ContactReason.SpecificPerson], [new EmailAddress("jim.brown@gmail.com")])
-        //    )
-        //),
-        //new (
-        //    new Name("Peter", "Johnson"),
-        //    Role.VenueCoordinator,
-        //    new ContactDetails(
-        //        new ContactOptions([ContactReason.DietaryRequirements], [new EmailAddress("peter.johnson@gmail.com")])
-        //    )
-        //),
     ]; 
     
     private NotablePerson GetPersonByRole(Role role) => NotablePeople.First(p => p.Role == role);
@@ -252,9 +166,7 @@ public sealed class WeddingDetails : IWeddingDetails
         ]
     );
     
-
     public IEnumerable<Event> Events { get; }
-
     
     public DressCode DressCode { get; } 
         = new DressCode(
@@ -304,9 +216,4 @@ public sealed class WeddingDetails : IWeddingDetails
             new BigGalleryItem(new WebsiteImage("https://pbs.twimg.com/media/EbN2CI3WAAcVxXD?format=jpg&name=large", null), "", "Spongebob Scooby", "Who made this...")
         ]
     );
-    
-    public WebsiteLink RegistryLink { get; }
-        = new WebsiteLink("https://youtu.be/dQw4w9WgXcQ");
-        
-    //public UsersCanAddGuests UsersCanAddGuests => UsersCanAddGuests.ContactUs;
 }
