990fd6df03168e8026a88d7bfa8e01a27d6dc054
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-02-05 23:41:59 +0000

    Add method within RSVP questions to give a summary string of the user's response

--- WeddingWebsite/Models/Rsvp/RsvpQuestionType.cs
+++ WeddingWebsite/Models/Rsvp/RsvpQuestionType.cs
@@ -8,6 +8,11 @@ public abstract record RsvpQuestionType
         {
             return [DataColumn];
         }
+        
+        public override string? GetAnswerString(IReadOnlyList<string?> data)
+        {
+            return data.ElementAtOrDefault(DataColumn.Id);
+        }
     }
 
     public sealed record Select(RsvpDataColumn DataColumn, IEnumerable<String> Options, FreeText? OtherField) : RsvpQuestionType
@@ -15,6 +20,11 @@ public abstract record RsvpQuestionType
         public override IEnumerable<RsvpDataColumn> GetAllColumns()
         {
             return [DataColumn];
+        }
+        
+        public override string? GetAnswerString(IReadOnlyList<string?> data)
+        {
+            return data.ElementAtOrDefault(DataColumn.Id);
         }
     }
 
@@ -31,7 +41,26 @@ public abstract record RsvpQuestionType
                 return Options.Select(option => option.DataColumn);
             }
         }
+        
+        public override string? GetAnswerString(IReadOnlyList<string?> data)
+        {
+            var selectedOptions = Options
+                .Where(option => data.ElementAtOrDefault(option.DataColumn.Id) == "Y")
+                .Select(option => option.Option);
+            
+            if (OtherField != null)
+            {
+                var otherValue = data.ElementAtOrDefault(OtherField.DataColumn.Id);
+                if (!string.IsNullOrEmpty(otherValue))
+                {
+                    selectedOptions = selectedOptions.Append(otherValue);
+                }
+            }
+            
+            return selectedOptions.Any() ? string.Join(", ", selectedOptions) : "No options selected";
+        }
     }
 
     public abstract IEnumerable<RsvpDataColumn> GetAllColumns();
+    public abstract string? GetAnswerString(IReadOnlyList<string?> data);
 }
