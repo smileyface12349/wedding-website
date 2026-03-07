a5990e349d0949025e3459151b60f365303754f4
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-03-07 10:20:19 +0000

    Fix outdated documentation implying that column id 0 is not allowed

--- WeddingWebsite/Models/Rsvp/RsvpDataColumn.cs
+++ WeddingWebsite/Models/Rsvp/RsvpDataColumn.cs
@@ -1,7 +1,7 @@
 ﻿namespace WeddingWebsite.Models.Rsvp;
 
 /// <summary>
-/// Contains a place to store a piece of data in a response to RSVP form. Id must be between 1 and 20 - there are
+/// Contains a place to store a piece of data in a response to RSVP form. Id must be between 0 and 20 - there are
 /// exactly 20 boxes to store the data in. It's really important that this isn't changed after RSVPs are opened.
 /// The display name is used for your benefit on the admin site within the guests table - you can sort and filter by
 /// each column. If you leave it as null, it won't be shown on this table but can still be viewed when looking at
