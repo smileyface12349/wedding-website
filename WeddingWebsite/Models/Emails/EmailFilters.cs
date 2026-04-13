4afddd7853b9d9ee73b29c8bb2fc3ab4e6c533ca
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-04-13 18:16:53 +0100

    Add record to encapsulate email filters

---
+++ WeddingWebsite/Models/Emails/EmailFilters.cs
@@ -0,0 +1,27 @@
+﻿using WeddingWebsite.Models.Accounts;
+
+namespace WeddingWebsite.Models.Emails;
+
+public record EmailFilters(
+    NumGuestsFilter NumGuestsFilter = NumGuestsFilter.Any,
+    RsvpStatusFilter RsvpStatusFilter = RsvpStatusFilter.Any,
+    bool RsvpStatusMatchAll = false
+)
+{
+    public bool GuestMatchesRsvpStatus(Guest guest)
+    {
+        switch (RsvpStatusFilter)
+        {
+            case RsvpStatusFilter.NotNo:
+                return guest.Rsvp != RsvpStatus.No;
+            case RsvpStatusFilter.No:
+                return guest.Rsvp == RsvpStatus.No;
+            case RsvpStatusFilter.Waiting:
+                return guest.Rsvp == RsvpStatus.NotResponded;
+            case RsvpStatusFilter.Yes:
+                return guest.Rsvp == RsvpStatus.Yes;
+        }
+
+        return true;
+    }
+}
