2848ed0d2225accd3ba2296aa3a01b431002b791
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-04-13 18:19:00 +0100

    Add email variables for email address and username

---
+++ WeddingWebsite/Models/Emails/Variables/EmailEmailVariable.cs
@@ -0,0 +1,15 @@
+﻿using WeddingWebsite.Data.Models;
+
+namespace WeddingWebsite.Models.Emails.Variables;
+
+public class EmailEmailVariable : EmailVariable
+{
+    public override string Pattern => "EMAIL";
+    public override string Example => "bob@example.com";
+    public override string Description => "Recipient email address.";
+    
+    public override string GetValue(AccountWithGuests account, EmailFilters filters, string args)
+    {
+        return account.Email ?? "";
+    }
+}
