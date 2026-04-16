d49b6a1a21105e559ba83317779a7d7eb5e106f6
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-04-16 23:02:29 +0100

    Add database migration with new created at column

---
+++ WeddingWebsite/Data/Migrations/20260416215328_AddTimestampToTodoList.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestampToTodoList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                type: "INTEGER", 
+                name: "CreatedAt", 
+                table: "TodoItems", 
+                nullable: false, 
+                defaultValue: 0
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Remove column not supported so we need to manually copy the data over
+            
+            migrationBuilder.CreateTable(
+                name: "OldTodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<string>(type: "TEXT", nullable: true),
+                    GroupId = table.Column<string>(type: "TEXT", nullable: true),
+                    Text = table.Column<string>(type: "TEXT", nullable: true),
+                    WaitingUntil = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CompletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                    table.ForeignKey("FK_TodoItems_AspNetUsers_OwnerId", x => x.OwnerId, "AspNetUsers", "Id");
+                    table.ForeignKey("FK_TodoItems_TodoGroups_GroupId", x => x.GroupId, "TodoGroups", "Id");
+            });
+
+            migrationBuilder.Sql(@"
+                INSERT INTO OldTodoItems (Id, OwnerId, GroupId, Text, WaitingUntil, CompletedAt)
+                SELECT Id, OwnerId, GroupId, Text, WaitingUntil, CompletedAt
+                FROM TodoItems
+            ");
+            
+            migrationBuilder.DropTable("TodoItems");
+            migrationBuilder.RenameTable("OldTodoItems", newName: "TodoItems");
+        }
+    }
+}
