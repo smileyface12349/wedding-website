b09e6107983bec8add63fdd4dd67693324a55b39
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-06-18 10:34:07 +0100

    Supply default config property to use while the customised config is loading

--- WeddingWebsite/Services/IConfigProvider.cs
+++ WeddingWebsite/Services/IConfigProvider.cs
@@ -1,9 +1,23 @@
-﻿using WeddingWebsite.Models.ConfigInterfaces;
+﻿using WeddingWebsite.Config;
+using WeddingWebsite.Models.ConfigInterfaces;
 
 namespace WeddingWebsite.Services;
 
 public interface IConfigProvider
 {
+    /// <summary>
+    /// Get the website configuration for the current user.
+    /// </summary>
     IWebsiteConfig GetConfig();
+    
+    /// <summary>
+    /// Get the wedding details for the current user.
+    /// </summary>
     IWeddingDetails GetDetails();
+    
+    /// <summary>
+    /// Quick access for the default website configuration. This may be used while the specific config is still loading.
+    /// No equivalent property is provided for wedding details - this would potentially leak sensitive information.
+    /// </summary>
+    static IWebsiteConfig DefaultConfig => ConfigChoices.ActiveConfig.Theme;
 }
