af37da987e73198d447030042e617edb4db978c1
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-06-29 10:35:57 +0100

    Add lift sharing service with add lift method only

---
+++ WeddingWebsite/Services/ILiftSharingService.cs
@@ -0,0 +1,8 @@
+﻿using WeddingWebsite.Models.LiftSharing;
+
+namespace WeddingWebsite.Services;
+
+public interface ILiftSharingService
+{
+    void AddLift(string userId, string name, int spaces, Journey journey, string notes);
+}
---
+++ WeddingWebsite/Services/LiftSharingService.cs
@@ -0,0 +1,24 @@
+﻿using WeddingWebsite.Data.Stores;
+using WeddingWebsite.Models.LiftSharing;
+
+namespace WeddingWebsite.Services;
+
+public class LiftSharingService(ILiftSharingStore store) : ILiftSharingService
+{
+    public void AddLift(string userId, string name, int spaces, Journey journey, string notes)
+    {
+        var lift = new SharedLift(
+            Id: Guid.NewGuid().ToString(),
+            UserId: userId,
+            UserEmail: "unused",
+            Name: name,
+            Spaces: spaces,
+            NumBookings: 0,
+            Start: journey.Start,
+            End: journey.End,
+            Notes: notes
+        );
+        
+        store.AddLift(lift);
+    }
+}
