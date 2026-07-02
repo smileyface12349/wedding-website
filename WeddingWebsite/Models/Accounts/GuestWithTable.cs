6d5d75a2d11b6ffb26c3238611218cde874a7919
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-07-02 16:03:48 +0100

    Add new model for guest with table assignment

---
+++ WeddingWebsite/Models/Accounts/GuestWithTable.cs
@@ -0,0 +1,11 @@
+﻿using WeddingWebsite.Data.Models;
+using WeddingWebsite.Models.People;
+
+namespace WeddingWebsite.Models.Accounts;
+
+public record GuestWithTable(
+    string Id, 
+    Name Name,
+    RsvpStatus RsvpStatus,
+    SeatingPlanTable? Table
+) : GuestWithId(Id, Name, RsvpStatus);
