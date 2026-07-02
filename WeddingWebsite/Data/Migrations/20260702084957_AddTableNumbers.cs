dda7a36be21c4c045078f26542064fe1d2207ddb
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-07-02 10:05:36 +0100

    Add seating plan database migration

---
+++ WeddingWebsite/Data/Migrations/20260702084957_AddTableNumbers.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableNumbers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SeatingPlan",
+                columns: table => new
+                {
+                    GuestId = table.Column<string>(type: "TEXT", nullable: false),
+                    TableId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeatingPlan", x => new { x.GuestId });
+                    table.ForeignKey(
+                        name: "FK_SeatingPlan_Guests_GuestId",
+                        column: x => x.GuestId,
+                        principalTable: "Guests",
+                        principalColumn: "GuestId",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                    table.ForeignKey(
+                        name: "FK_SeatingPlan_SeatingPlanTables_TableId",
+                        column: x => x.TableId,
+                        principalTable: "SeatingPlanTables",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                }
+            );
+
+            migrationBuilder.CreateTable(
+                name: "SeatingPlanTables",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeatingPlanTables", x => x.Id);
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("SeatingPlan");
+            migrationBuilder.DropTable("SeatingPlanTables");
+        }
+    }
+}
