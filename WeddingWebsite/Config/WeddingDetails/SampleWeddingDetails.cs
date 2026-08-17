fd25c2dcb46b7920f89b4ccfe77cf860d5384329
smileyface12349 <josh.humphriss.signup@gmail.coom>
2026-08-17 13:01:09 +0100

    Remove old gallery item constructor as sizes are now required for correct functionality

--- WeddingWebsite/Config/WeddingDetails/SampleWeddingDetails.cs
+++ WeddingWebsite/Config/WeddingDetails/SampleWeddingDetails.cs
@@ -296,12 +296,12 @@ public sealed class SampleWeddingDetails : IWeddingDetails
         [
             new GallerySection(
                 [
-                    new GalleryItem("https://pm1.aminoapps.com/6549/18b7f2ae94d82dbe03c54e4e8de0f17211236d70_hq.jpg"),
-                    new GalleryItem("https://i.ytimg.com/vi/GAyzLbpZeKE/maxresdefault.jpg"),
-                    new GalleryItem("https://ih1.redbubble.net/image.5821996399.7493/fposter,small,wall_texture,square_product,600x600.jpg"),
-                    new GalleryItem("https://pbs.twimg.com/media/EbN2CI3WAAcVxXD?format=jpg&name=large"),
-                    new GalleryItem("https://pbs.twimg.com/media/GfagNwOWQAAFWlB?format=jpg&name=medium"),
-                    new GalleryItem("https://i.ytimg.com/vi/9tcHOMOVfrk/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDJ8fvK_Ob-YZ66NKdIqKydgxvhZQ"),
+                    new GalleryItem(new WebsiteImage("https://pm1.aminoapps.com/6549/18b7f2ae94d82dbe03c54e4e8de0f17211236d70_hq.jpg")),
+                    new GalleryItem(new WebsiteImage("https://i.ytimg.com/vi/GAyzLbpZeKE/maxresdefault.jpg")),
+                    new GalleryItem(new WebsiteImage("https://ih1.redbubble.net/image.5821996399.7493/fposter,small,wall_texture,square_product,600x600.jpg")),
+                    new GalleryItem(new WebsiteImage("https://pbs.twimg.com/media/EbN2CI3WAAcVxXD?format=jpg&name=large")),
+                    new GalleryItem(new WebsiteImage("https://pbs.twimg.com/media/GfagNwOWQAAFWlB?format=jpg&name=medium")),
+                    new GalleryItem(new WebsiteImage("https://i.ytimg.com/vi/9tcHOMOVfrk/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDJ8fvK_Ob-YZ66NKdIqKydgxvhZQ")),
                 ], 
                 "General Pictures", 
                 "Aren't they having such a happy life together..."
